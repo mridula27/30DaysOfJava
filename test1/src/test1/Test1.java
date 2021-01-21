78cb847532cd896a9308a237cbfdac18be7ca873
Mridu <talk2mridula@gmail.com>
2021-01-21 18:14:48 -0500

    initial commit

---
+++ test1/src/test1/Test1.java
@@ -0,0 +1,19 @@
+package test1;
+
+import java.util.Scanner;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("What is your favorite restaurant? ");
+		Scanner in = new Scanner(System.in);
+		String s = in.nextLine();
+		in.close();
+		System.out.print(s);
+		
+				
+		
+	}
+
+}
